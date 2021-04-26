2aa46bf792978c7cf988ab527d13f1e03bda39cc
Giorgos Apostolopoulos <s3apostol@gmail.com>
2021-04-26 19:41:39 +0300

    Create data management fragment

--- app/src/main/java/com/tripple_d/mycoolsportsapp/ui/data/DataFragment.kt
+++ app/src/main/java/com/tripple_d/mycoolsportsapp/ui/data/DataFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.tripple_d.mycoolsportsapp.R
 
 class DataFragment : Fragment() {
@@ -22,10 +21,12 @@ class DataFragment : Fragment() {
         dataViewModel =
             ViewModelProvider(this).get(DataViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_data, container, false)
-        val textView: TextView = root.findViewById(R.id.text_data)
-        dataViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val recyclerView = root.findViewById<RecyclerView>(R.id.rvDataManagement)
+        recyclerView.apply {
+            adapter = DataListAdapter(
+                mutableListOf<String>("Αθλήματα", "Ομάδες", "Αθλητές", "Αγώνες")
+            )
+        }
         return root
     }
 }
