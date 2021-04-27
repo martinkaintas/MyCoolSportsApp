44981bfe5d23bc68812672b3990160312fd22111
Kalovelo <apostoloskalovelonis@gmail.com>
2021-04-27 23:36:47 +0300

    feat(matchDetails): use greek orthodox styling

--- app/src/main/java/com/tripple_d/mycoolsportsapp/ui/match_details/MatchDetailsFragment.kt
+++ app/src/main/java/com/tripple_d/mycoolsportsapp/ui/match_details/MatchDetailsFragment.kt
@@ -1,34 +1,62 @@
 package com.tripple_d.mycoolsportsapp.ui.match_details
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.tripple_d.mycoolsportsapp.R
 import com.tripple_d.mycoolsportsapp.models.Match
-import com.tripple_d.mycoolsportsapp.ui.home.MatchAdapter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MatchDetailsFragment(val match: Match): Fragment() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
+
         val root = inflater.inflate(R.layout.fragment_match_details, container, false)
         root.findViewById<TextView>(R.id.tvMatchSport).text = match.sport.name
         root.findViewById<TextView>(R.id.tvMatchDetailsPlace).text = match.city + ", " + match.country
-        root.findViewById<TextView>(R.id.tvMatchDetailsDate).text = match.date.toString()
+        root.findViewById<TextView>(R.id.tvMatchDetailsDate).text = formatDate(match.date)
+
+
+        val sortedParticipants = match.participants.sortedWith(compareByDescending({ it.score })) //sort
 
         val recyclerView = root.findViewById<RecyclerView>(R.id.rvParticipant)
-        val participantAdapter = ParticipantAdapter(match.participants)
+        val participantAdapter = ParticipantAdapter(sortedParticipants)
         recyclerView.apply {
             adapter = participantAdapter
         }
         return root
+    }
+
+    private fun getGreekDay(day:String):String{
+        when (day) {
+            "MONDAY"-> return "Δευτέρα"
+            "TUESDAY"-> return "Τρίτη"
+            "WEDNESDAY"-> return "Τετάρτη"
+            "THURSDAY"-> return "Πέμπτη"
+            "FRIDAY"-> return "Παρασκευή"
+            "SATURDAY"-> return "Σάββατο"
+            }
+        return "Κυριακή"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun formatDate(date:LocalDateTime):String{
+        var day = getGreekDay(date.dayOfWeek.toString())
+        var formatter = DateTimeFormatter.ofPattern("dd/MM - HH:mm")
+        var calendarDate = date.format(formatter)
+        return day + " " + calendarDate
     }
 }
 
